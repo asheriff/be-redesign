c9a4a749f1c925195dd9e571266580f32f0d21f4
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-06-09 11:42:00 -0500

    Added kwicks plugin, layout for subnav

--- controller/main.rb
+++ controller/main.rb
@@ -1,4 +1,8 @@
 class MainController < Controller
   def index
   end
+  
+  def bsa__basics(index=0)
+    @selected_index = index.to_i
+  end
 end
--- public/js/site.js
+++ public/js/site.js
@@ -14,8 +14,35 @@
   "use strict";
   
   $(document).ready(function(){
+    
+    // Superfish Dropdown Menu
+    //
     $('ul.sf-menu LI:first-child, ul.sf-menu LI:first-child>A').addClass("first");
     $('ul.sf-menu LI:last-child, ul.sf-menu LI:last-child>A').addClass("last");
     $("UL.sf-menu").superfish();
+    
+    // Kwicks Slider Menu
+    //
+    var $menu = $('.kwicks-menu');
+    var width = $menu.width();
+    var count = $menu.find(">LI").length;
+    
+    $menu.find(">LI").css( "width", width/count );
+    
+    $menu.find(">LI.selected").prev().addClass("before_selected");
+    $menu.find(">LI.selected").next().addClass("after_selected");
+    
+    // TODO: Don't bother to init this if text on all tabs is 100%visible.
+    // TODO: This lib only allows a single config.max for every item.  Probably
+    //       the best thing to do is set max to max content width of all items.
+    // IMPROVE: The jquery.kwicks.js lib would be better if it used hoverIntent.
+    
+    // return;
+    
+    $menu.kwicks({
+      max : 150,
+      spacing : 0
+    });
+    
   });
 })(jQuery);
