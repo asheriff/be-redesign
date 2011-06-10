4ed1fffe07b0e191da5e430c94c96ccace1389d9
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-06-10 09:37:57 -0500

    Removed the EM in the kwicks subnav

--- public/js/site.js
+++ public/js/site.js
@@ -35,9 +35,6 @@
     var ul_width = $menu.width();
     var count = $menu_items.length;
     
-    $menu_items.find(">A").wrapInner( "<EM/>" );
-    
-    
     // Add some classes so we can apply the bottom border-radius to the next+prev
     // elems to fake the tab look in decent browsers.
     $menu.find(">LI.selected").prev().addClass("before_selected");
@@ -47,37 +44,30 @@
     // Find the total width of all the LIs.  If less than the parent UL's width
     // just quit.
     var total_li_width = 0;
-    for(var i=0; i<count; i++){
+    for( var i=0; i<count; i++ ){
       total_li_width += $menu_items.eq(i).width();
     }
     if( total_li_width <= ul_width ){ return; }
     
-    $menu_items.css( "width", ul_width/count );
     
-    
-    // Find the widest text within the LI elems.  We'll use the decendent EM elem
-    // so that...
+    // Find the widest LI item to use that to set the max option in the kwicks
+    // setup options.
     var max_item_width = 0;
-    var widest_item = null;
-    for( var i=0; i<count; i++){
-      var item_width = $menu_items.eq(i).find("EM").width();
+    for( var i=0; i<count; i++ ){
+      var item_width = $menu_items.eq(i).width();
       if( item_width > max_item_width ){
         max_item_width = item_width;
-        widest_item = $menu_items.eq(i);
       }
     }
     
-    // Now add the parent border+padding.  There's likely a better way to do this.
-    max_item_width +=
-      parseInt( widest_item.find("A").css("border-right-width") ) +
-      parseInt( widest_item.find("EM").css("padding-left") ) +
-      parseInt( widest_item.find("EM").css("padding-right") )
-    ;
+    
+    // Now set the LI item widths.
+    $menu_items.css( "width", ul_width/count );
+    
     
     // Init the kwicks plugin.
     $menu.kwicks({
-      max: max_item_width,
-      spacing: 0
+      max: max_item_width
     });
     
   });
