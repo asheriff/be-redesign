a67f694ea4a5bfd571f9fe8da3368323cb76f46b
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-06-10 09:14:03 -0500

    Use different number of subnav links on the example pages

--- controller/main.rb
+++ controller/main.rb
@@ -2,7 +2,36 @@ class MainController < Controller
   def index
   end
   
+  def bsa__about(index=0)
+    @selected_index = index.to_i
+    @subnav = subnav
+    @page = "bsa/about"
+  end
+  
   def bsa__basics(index=0)
     @selected_index = index.to_i
+    @subnav = subnav(5)
+    @page = "bsa/basics"
   end
+  
+  def bsa__overview(index=0)
+    @selected_index = index.to_i
+    @subnav = subnav(10)
+    @page = "bsa/overview"
+  end
+  
+  private
+  
+    def subnav(dummy_items=0)
+      links = [
+        "Overview",
+        "This Link Should Have The Very Longest Label"
+      ]
+      
+      dummy_items.times{ links << Faker::Lorem.words( rand(4)+2 ).map{ |w| w.capitalize }.join(" ") }
+      
+      links << "Last Item In List"
+      
+      links
+    end
 end
